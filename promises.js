8f53769b28270245303ad07a0de2ab459a1366ad
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-09-14 16:25:17 +0100

    Promise chaining playground commit

--- promises.js
+++ promises.js
@@ -1,14 +1,48 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //resolve([7, 4, 1]);
-    reject("Things went wrong");
-  }, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //resolve([7, 4, 1]);
+//     reject("Things went wrong");
+//   }, 2000);
+// });
 
-doWorkPromise
-  .then((result) => {
-    console.log("Success", result);
+// doWorkPromise
+//   .then((result) => {
+//     console.log("Success", result);
+//   })
+//   .catch((error) => {
+//     console.log("Error", error);
+//   });
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+//Without promise chaining
+add(1, 2).then((sum) => {
+  console.log(sum);
+});
+add(sum, 5)
+  .then((sum2) => {
+    console.log(sum2);
   })
-  .catch((error) => {
-    console.log("Error", error);
+  .catch((e) => console.log(e))
+  .catch((e) => {
+    console.log(e);
+  });
+
+//Using promise chaining
+add(1, 1)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+  })
+  .catch((e) => {
+    console.log(sum2);
   });
